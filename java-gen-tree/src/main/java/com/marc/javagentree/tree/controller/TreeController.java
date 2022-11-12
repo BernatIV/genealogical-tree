d871a6fb9a4f855f045864f41171b9be59df4151
BernatIV <marc.mane.duatis@gmail.com>
2022-11-13 00:24:53 +0100

    Fix CORS Missing Allow Origin request error

--- java-gen-tree/src/main/java/com/marc/javagentree/tree/controller/TreeController.java
+++ java-gen-tree/src/main/java/com/marc/javagentree/tree/controller/TreeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://arbregenealogic.com/")
 @RestController
 @RequestMapping("api/tree")
 public class TreeController {
